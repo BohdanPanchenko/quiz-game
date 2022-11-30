9d82e96e7f0e9765549c3d6bf936757a3d9880ed
andreyx3x2 <64372689+andreyx3x2@users.noreply.github.com>
2022-11-30 17:54:08 +0200

    update

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -2,8 +2,23 @@ import "./QuestionPage.css";
 const QuestionPage = (props) => {
   return (
     <div className="question-container">
-      <div className="question-timer"></div>
-      <div className="question-body"></div>
+      <div className="question-timer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          width="108px"
+          height="108px"
+        >
+          <defs>
+            <linearGradient id="GradientColor">
+              <stop offset="0%" stopColor="#e91e63" />
+              <stop offset="100%" stopColor="#673ab7" />
+            </linearGradient>
+          </defs>
+          <circle cx="54" cy="54" r="50" strokeLinecap="round" />
+        </svg>
+      </div>
+      <div className="question-body">{props.questions.question}</div>
       <ul className="question-options">
         {props.questions.options.map((el, index) => {
           return (
@@ -16,7 +31,14 @@ const QuestionPage = (props) => {
           );
         })}
       </ul>
-      <div className="question-progress"></div>
+      <div className="question-progress">
+        <div className="question-number">
+          {props.currentQuestion + " of " + props.questions.length}
+        </div>
+        <div className="progress-bar" style={{ width: props.stage }}>
+          <span></span>
+        </div>
+      </div>
     </div>
   );
 };
