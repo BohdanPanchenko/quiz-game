74a8b45b5f7893d5ad4702106da972dbbf09d28b
bohdan panchenko <logitechchillstream@gmail.com>
2022-12-02 00:21:39 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,66 +3,68 @@ import Quiz from "./components/Quiz";
 import StartPage from "./components/StartPage";
 import QuestionPage from "./components/QuestionPage";
 import React from "react";
-function App() {
-  const questions = [
-    {
-      question: "Lorem asd asda sasd da  s",
-      options: [
-        { marker: "A", content: "asdadadadsda", isCorrect: true },
-        { marker: "B", content: "asdadadadsda", isCorrect: false },
-        { marker: "C", content: "asdadadadsda", isCorrect: false },
-        { marker: "D", content: "asdadadadsda", isCorrect: false },
-      ],
-    },
-    {
-      question: "Lorem asd asda sasd da  s",
-      options: [
-        { marker: "A", content: "asdadadadsda", isCorrect: true },
-        { marker: "B", content: "asdadadadsda", isCorrect: false },
-        { marker: "C", content: "asdadadadsda", isCorrect: false },
-        { marker: "D", content: "asdadadadsda", isCorrect: false },
-      ],
-    },
-    {
-      question: "Lorem asd asda sasd da  s",
-      options: [
-        { marker: "A", content: "asdadadadsda", isCorrect: true },
-        { marker: "B", content: "asdadadadsda", isCorrect: false },
-        { marker: "C", content: "asdadadadsda", isCorrect: false },
-        { marker: "D", content: "asdadadadsda", isCorrect: false },
-      ],
-    },
-    {
-      question: "Lorem asd asda sasd da  s",
-      options: [
-        { marker: "A", content: "asdadadadsda", isCorrect: true },
-        { marker: "B", content: "asdadadadsda", isCorrect: false },
-        { marker: "C", content: "asdadadadsda", isCorrect: false },
-        { marker: "D", content: "asdadadadsda", isCorrect: false },
-      ],
-    },
-  ];
-  const [gameStarted, setGameStarted] = React.useState(false);
-  const [currentQuestion, setCurrentQuestion] = React.useState(null);
-  function startGame() {
-    setGameStarted((prev) => !prev);
 
-    setCurrentQuestion(() => 0);
-  }
-  return (
-    <div className="App">
-      <Quiz>
-        {!gameStarted && <StartPage OnGameStart={startGame} />}
-        {gameStarted && (
-          <QuestionPage
-            questions={questions[currentQuestion]}
-            currentQuestion={currentQuestion}
-            questionsNumber={questions.length}
-          />
-        )}
-      </Quiz>
-    </div>
-  );
+function App() {
+    const questions = [{
+            question: "how do you do, fella kids?",
+            options: [
+                { marker: "A", content: "asdadadadsda", isCorrect: true },
+                { marker: "B", content: "asdadadadsda", isCorrect: false },
+                { marker: "C", content: "asdadadadsda", isCorrect: false },
+                { marker: "D", content: "asdadadadsda", isCorrect: false },
+            ],
+        },
+        {
+            question: "Lorem asd asda sasd da  s",
+            options: [
+                { marker: "A", content: "asdadadadsda", isCorrect: true },
+                { marker: "B", content: "asdadadadsda", isCorrect: false },
+                { marker: "C", content: "asdadadadsda", isCorrect: false },
+                { marker: "D", content: "asdadadadsda", isCorrect: false },
+            ],
+        },
+        {
+            question: "Lorem asd asda sasd da  s",
+            options: [
+                { marker: "A", content: "asdadadadsda", isCorrect: true },
+                { marker: "B", content: "asdadadadsda", isCorrect: false },
+                { marker: "C", content: "asdadadadsda", isCorrect: false },
+                { marker: "D", content: "asdadadadsda", isCorrect: false },
+            ],
+        },
+        {
+            question: "Lorem asd asda sasd da  s",
+            options: [
+                { marker: "A", content: "asdadadadsda", isCorrect: true },
+                { marker: "B", content: "asdadadadsda", isCorrect: false },
+                { marker: "C", content: "asdadadadsda", isCorrect: false },
+                { marker: "D", content: "asdadadadsda", isCorrect: false },
+            ],
+        },
+    ];
+    const [gameStarted, setGameStarted] = React.useState(false);
+    const [currentQuestion, setCurrentQuestion] = React.useState(null);
+
+    function startGame() {
+        setGameStarted((prev) => !prev);
+
+        setCurrentQuestion(() => 0);
+    }
+    return ( <
+        div className = "App" >
+        <
+        Quiz > {!gameStarted && < StartPage OnGameStart = { startGame }
+            />} {
+            gameStarted && ( <
+                QuestionPage questions = { questions[currentQuestion] }
+                currentQuestion = { currentQuestion }
+                questionsNumber = { questions.length }
+                />
+            )
+        } <
+        /Quiz> < /
+        div >
+    );
 }
 
 export default App;
